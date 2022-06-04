d5a077003717b2309127bfd8259269d94baaed11
Mateusz Ciszek <cichu32@gmail.com>
2022-06-04 15:46:51 +0200

    Initial commit

---
+++ src/main/kotlin/com/github/cichu/auto_replanter/AutoReplanter.kt
@@ -0,0 +1,12 @@
+package com.github.cichu.auto_replanter
+
+import net.fabricmc.api.ModInitializer
+
+@Suppress("UNUSED")
+object AutoReplanter : ModInitializer {
+
+	private const val MOD_ID = "auto_replanter"
+
+	override fun onInitialize() {
+	}
+}
---
+++ src/main/kotlin/com/github/cichu/auto_replanter/mixins/CropBlockMixin.java
@@ -0,0 +1,105 @@
+package com.github.cichu.auto_replanter.mixins;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.CropBlock;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(CropBlock.class)
+public class CropBlockMixin extends AbstractBlockMixin {
+
+    // FIXME: fix used tools nad loot drops for all crops
+    @Override
+    protected void injectAtHeadIntoOnUseMethod(
+            BlockState state,
+            World world,
+            BlockPos pos,
+            PlayerEntity player,
+            Hand hand,
+            BlockHitResult hit,
+            CallbackInfoReturnable<ActionResult> callbackInfo) {
+        handleOnUseHarvesting(state, world, pos, player, hand, hit, callbackInfo);
+    }
+
+    private void handleOnUseHarvesting(
+            BlockState state,
+            World world,
+            BlockPos pos,
+            PlayerEntity player,
+            Hand hand, BlockHitResult hit,
+            CallbackInfoReturnable<ActionResult> callbackInfo) {
+        ItemStack stackInHand = player.getStackInHand(hand);
+        if (isFullyGrown(state) && stackInHand.isOf(getPickUpTool())) {
+            if (!world.isClient) {
+                harvestAndReplantCrop(world, pos, player, hand, hit, stackInHand);
+            }
+            callbackInfo.setReturnValue(ActionResult.success(world.isClient));
+            callbackInfo.cancel();
+        }
+    }
+
+    private boolean isFullyGrown(BlockState state) {
+        return getThisInstance().isMature(state);
+    }
+
+    private Item getPickUpTool() {
+        return Items.SHEARS;
+    }
+
+    private void harvestAndReplantCrop(World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, ItemStack stackInHand) {
+        playSound(world, pos);
+        updateCropState(world, pos);
+        dropLoot(world, pos, player, hit);
+        updateToolState(world, pos, player, hand, stackInHand);
+    }
+
+    private CropBlock getThisInstance() {
+        return (CropBlock) (Object) this;
+    }
+
+    private void playSound(World world, BlockPos pos) {
+        world.playSound(null, pos, SoundEvents.ENTITY_SHEEP_SHEAR, SoundCategory.BLOCKS, 1, 1);
+    }
+
+    private void updateCropState(World world, BlockPos pos) {
+        world.setBlockState(pos, getThisInstance().withAge(0), Block.NOTIFY_LISTENERS);
+    }
+
+    private void dropLoot(World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
+        Direction direction = hit.getSide();
+        Direction direction2 = direction.getAxis() == Direction.Axis.Y ? player.getHorizontalFacing().getOpposite() : direction;
+        double x = (double) pos.getX() + 0.5 + (double) direction2.getOffsetX() * 0.65;
+        double y = (double) pos.getY() + 0.1;
+        double z = (double) pos.getZ() + 0.5 + (double) direction2.getOffsetZ() * 0.65;
+        // FIXME: make number of dropped seeds variable
+        dropItem(world, x, y, z, Items.WHEAT_SEEDS, 4);
+        dropItem(world, x, y, z, Items.WHEAT, 1);
+    }
+
+    private void dropItem(World world, double x, double y, double z, Item item, int count) {
+        ItemEntity crop = new ItemEntity(world, x, y, z, new ItemStack(item, count));
+        world.spawnEntity(crop);
+    }
+
+    private void updateToolState(World world, BlockPos pos, PlayerEntity player, Hand hand, ItemStack tool) {
+        tool.damage(1, player, playerEntity -> playerEntity.sendToolBreakStatus(hand));
+        world.emitGameEvent(player, GameEvent.SHEAR, pos);
+        player.incrementStat(Stats.USED.getOrCreateStat(getPickUpTool()));
+    }
+}
